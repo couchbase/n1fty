5ca90b0d1b7b2131ccb5b1bd99895618f05bbb6b
abhinavdangeti <abhinav@couchbase.com>
2019-02-04 09:59:17 -0800

    Include query prep work in search, and TODOs
    Change-Id: I2bca883450006ffe0ca13ab3db767a8983fcf4d7
    Reviewed-on: http://review.couchbase.org/104467
    Well-Formed: Build Bot <build@couchbase.com>
    Reviewed-by: Abhinav Dangeti <abhinav@couchbase.com>
    Tested-by: Abhinav Dangeti <abhinav@couchbase.com>

--- index.go
+++ index.go
@@ -12,13 +12,31 @@
 package n1fty
 
 import (
+	"context"
+	"encoding/base64"
+	"fmt"
+	"time"
+
+	"github.com/couchbase/cbauth"
+	pb "github.com/couchbase/cbft/protobuf"
 	"github.com/couchbase/cbgt"
 	"github.com/couchbase/query/datastore"
 	"github.com/couchbase/query/errors"
 	"github.com/couchbase/query/expression"
 	"github.com/couchbase/query/expression/parser"
 	"github.com/couchbase/query/timestamp"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
 )
+
+var DefaultGrpcConnectionIdleTimeout = time.Duration(60) * time.Second
+var DefaultGrpcConnectionHeartBeatInterval = time.Duration(60) * time.Second
+
+var DefaultGrpcMaxRecvMsgSize = 1024 * 1024 * 20 // 20 MB
+var DefaultGrpcMaxSendMsgSize = 1024 * 1024 * 20 // 20 MB
+
+// -----------------------------------------------------------------------------
 
 // Implements datastore.FTSIndex interface
 type FTSIndex struct {
@@ -122,7 +140,99 @@ func (i *FTSIndex) Scan(requestId string, span *datastore.Span, distinct bool,
 func (i *FTSIndex) Search(requestId string, searchInfo *datastore.FTSSearchInfo,
 	cons datastore.ScanConsistency, vector timestamp.Vector,
 	conn *datastore.IndexConnection) {
+	if conn == nil {
+		return
+	}
+
+	if searchInfo == nil {
+		conn.Error(errors.NewError(nil, "no search parameters provided"))
+		return
+	}
+
+	username, password, err := cbauth.GetHTTPServiceAuth(i.indexer.serverURL)
+	if err != nil {
+		conn.Error(errors.NewError(nil, "error fetching auth creds"))
+		return
+	}
+
+	options := []grpc.DialOption{
+		grpc.WithKeepaliveParams(keepalive.ClientParameters{
+			// timeout value for an inactive connection
+			Timeout: DefaultGrpcConnectionIdleTimeout,
+		}),
+		grpc.WithDefaultCallOptions(
+			grpc.MaxCallRecvMsgSize(DefaultGrpcMaxRecvMsgSize),
+			grpc.MaxCallSendMsgSize(DefaultGrpcMaxSendMsgSize),
+		),
+		grpc.WithPerRPCCredentials(&basicAuthCreds{
+			username: username,
+			password: password,
+		}),
+	}
+
+	grpcConn, err := grpc.Dial(i.indexer.serverURL, options...)
+	if err != nil {
+		conn.Error(errors.NewError(err, "could not connect to gRPC port"))
+		return
+	}
+	defer grpcConn.Close()
+
+	// create a new customer client
+	client := pb.NewSearchServiceClient(grpcConn)
+
+	query, err := PrepQueryBytes(searchInfo.Field.String(),
+		searchInfo.Query.String(),
+		searchInfo.Options.String())
+	if err != nil {
+		conn.Error(errors.NewError(err, ""))
+		return
+	}
+
+	searchRequest := &pb.SearchRequest{
+		Query:     query,
+		Stream:    true,
+		From:      searchInfo.Offset,
+		Size:      searchInfo.Limit,
+		IndexName: i.name,
+	}
+
+	stream, err := client.Search(context.Background(), searchRequest)
+	if err != nil || stream == nil {
+		conn.Error(errors.NewError(err, "search failed"))
+		return
+	}
+
+	fmt.Println(stream)
 	// FIXME
+}
+
+// -----------------------------------------------------------------------------
+
+// basicAuthCreds is an implementation of credentials.PerRPCCredentials
+// that transforms the username and password into a base64 encoded value
+// similar to HTTP Basic xxx
+type basicAuthCreds struct {
+	username string
+	password string
+}
+
+// GetRequestMetadata sets the value for "authorization" key
+func (b *basicAuthCreds) GetRequestMetadata(context.Context, ...string) (
+	map[string]string, error) {
+	return map[string]string{
+		"authorization": "Basic " + basicAuth(b.username, b.password),
+	}, nil
+}
+
+// RequireTransportSecurity should be true as even though the credentials
+// are base64, we want to have it encrypted over the wire.
+func (b *basicAuthCreds) RequireTransportSecurity() bool {
+	return false // TODO - make it true
+}
+
+func basicAuth(username, password string) string {
+	auth := username + ":" + password
+	return base64.StdEncoding.EncodeToString([]byte(auth))
 }
 
 // -----------------------------------------------------------------------------
