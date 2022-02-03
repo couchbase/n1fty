9ebba7dab734bf3925fc135266b70b0caff4c8f5
Abhinav Dangeti <abhinav@couchbase.com>
2022-02-03 16:57:26 -0700

    MB-51841: [BP] Close FTSIndexer's client when indexer is closed
    + This is so n1fty doesn't leak a client instance when an
      FTSIndexer is closed.
    
    (Reviewed-on: https://review.couchbase.org/c/n1fty/+/170053)
    
    Change-Id: Ifd5dadb2f25fa5b6d6b50764c906693d1e9d39ef
    Reviewed-on: https://review.couchbase.org/c/n1fty/+/173908
    Well-Formed: Restriction Checker
    Reviewed-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>
    Tested-by: Abhinav Dangeti <abhinav@couchbase.com>

--- indexer.go
+++ indexer.go
@@ -196,6 +196,9 @@ func (i *FTSIndexer) Close() error {
 		return nil
 	}
 	i.closed = true
+	if i.client != nil {
+		i.client.close()
+	}
 	i.m.Unlock()
 
 	i.cfg.unSubscribe(i.namespace + "$" + i.bucket + "$" + i.scope + "$" + i.keyspace)
